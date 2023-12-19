7e039a024d7fb9d44e1e7d48a224845ff2387778
Ivan Zustiak <ivan.zustiak@zurich.com>
2023-12-19 12:46:50 +0100

    add script for data processing and define paths

---
+++ data_preparation.py
@@ -0,0 +1,8 @@
+# %% imports
+import pandas as pd
+
+# %% paths and definitions
+
+# paths
+strPathData = 'c:/repositories/zzz_data_for_lstm/data/inputs/'
+strPathTemplates = 'c:/repositories/zzz_data_for_lstm/templates/'   
