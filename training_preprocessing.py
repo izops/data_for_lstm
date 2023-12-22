d4f8a71cd045efcc25b85ed9189d2a73920459aa
Ivan Zustiak <ivan.zustiak@zurich.com>
2023-12-22 08:11:17 +0100

    add function to process ingested json

--- training_preprocessing.py
+++ training_preprocessing.py
@@ -7,3 +7,41 @@ import string
 
 # paths
 strPathJSON = 'c:/repositories/zzz_data_for_lstm/data/outputs/'
+
+# file name
+strExtension = '.json'
+
+# functions
+def dtfJSONtoDataFrame(pobjJSONData: dict) -> pd.DataFrame:
+    """Process ingested JSON data to appropriate form for pandas data frame.
+
+    Inputs:
+        - pobjJSONData - data read in from a JSON file
+
+    Outputs:
+        - dtfOut - processed data frame that contains text column with the
+        source data, annotations column with all tokens, start column with
+        integer value of the starting position of the token, and end column
+        with integer value of the ending position of the token
+    """
+
+    assert type(pobjJSONData) == dict, 'Must be a dictionary from JSON input'
+
+    # process annotations first, normalize the text labels
+    dtfAnnotations = pd.json_normalize(
+        pobjJSONData,
+        'annotations',
+        meta='text',
+        sep='_'
+    )
+
+    # process the text data
+    dtfText = pd.DataFrame(pobjJSONData)
+
+    # drop annotations from the full table
+    dtfText.drop('annotations', axis=1, inplace=True)
+
+    # merge the two datasets to the final one
+    dtfOut = pd.merge(dtfText, dtfAnnotations, on='text')
+
+    return dtfOut
