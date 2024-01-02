b302e09828b545c9ebb7c03c205a2779f56eb977
Ivan Zustiak <ivan.zustiak@zurich.com>
2024-01-02 09:39:32 +0100

    attempt to import the data using multithreading

---
+++ multithread_training_preprocessing.py
@@ -0,0 +1,145 @@
+# %% imports
+import pandas as pd
+import os
+import json
+import string
+import concurrent.futures
+
+# %% definitions
+
+# paths
+strPathJSON = 'c:/repositories/zzz_data_for_lstm/data/outputs/'
+
+# file name
+strExtension = '.json'
+
+# functions
+def dtfJSONtoDataFrame(pobjJSONData: dict) -> pd.DataFrame:
+    """Process ingested JSON data to appropriate form for pandas data frame.
+
+    Inputs:
+        - pobjJSONData - data read in from a JSON file
+
+    Outputs:
+        - dtfOut - processed data frame that contains text column with the
+        source data, annotations column with all tokens, start column with
+        integer value of the starting position of the token, and end column
+        with integer value of the ending position of the token
+    """
+
+    assert type(pobjJSONData) == dict, 'Must be a dictionary from JSON input'
+
+    # process annotations first, normalize the text labels
+    dtfAnnotations = pd.json_normalize(
+        pobjJSONData,
+        'annotations',
+        meta='text',
+        sep='_'
+    )
+
+    # process the text data
+    dtfText = pd.DataFrame(pobjJSONData)
+
+    # drop annotations from the full table
+    dtfText.drop('annotations', axis=1, inplace=True)
+
+    # merge the two datasets to the final one
+    dtfOut = pd.merge(dtfText, dtfAnnotations, on='text')
+
+    return dtfOut
+
+def strCleanUpText(pstrText: str) -> str:
+    """Clean up the text from punctuation.
+    
+    Inputs:
+        - pstrText - original text to clean up
+
+    Outputs:
+        - strOut - text cleaned of all punctuation
+    """
+
+    assert type(pstrText) == str, 'String input required'
+
+    # define replacement dictionary
+    dctReplace = str.maketrans('', '', string.punctuation)
+
+    # remove punctuation from the input string
+    strOut = pstrText.translate(dctReplace)
+
+    return strOut
+
+def dtfProcessJSON(pstrPath: str):
+    assert os.path.isfile(pstrPath), 'Input must be a path to a file.'
+
+    # initialize an empty data frame
+    dtfProcessing = pd.DataFrame()
+
+    # process only JSON files
+    if pstrPath.endswith('.json'):
+        # open the file and ingest the data
+        with open(pstrPath) as objFile:
+            dctData = json.load(objFile)
+        
+        # consolidate the ingested data
+        dtfProcessing = dtfJSONtoDataFrame(dctData)
+
+        # replace punctuation from the original input text
+        dtfProcessing['clean_text'] = dtfProcessing['text'].apply(
+            strCleanUpText
+        )
+
+        # recalculate position of the labels after the cleanup
+        dtfProcessing['start_fix'] = dtfProcessing.apply(
+            lambda row: row['start'] - sum(
+                [
+                    1 for char in row['text'][:row['start']] \
+                    if char in string.punctuation
+                ]
+            ),
+            axis=1
+        )
+        dtfProcessing['end_fix'] = dtfProcessing.apply(
+            lambda row: row['end'] - sum(
+                [
+                    1 for char in row['text'][:row['end']] \
+                    if char in string.punctuation
+                ]
+            ),
+            axis=1
+        )
+
+        # drop redundant columns
+        dtfProcessing.drop(['text', 'start', 'end'], axis=1, inplace=True)
+
+    return dtfProcessing
+
+def dtfMergeDataFrames(plstDataFrames: list) -> pd.DataFrame:
+    assert type(plstDataFrames) == list, 'Input must be a list of data frames'
+
+    # concatenate all data frames from the input list to a single data frame
+    dtfOut = pd.concat(plstDataFrames, ignore_index=True)
+
+    return dtfOut
+
+def dtfThreading(pstrPath: str) -> pd.DataFrame:
+    # get all json files from the given directory
+    lstJSONFiles = [
+        os.path.join(
+            pstrPath,
+            strFile
+        ) for strFile in os.listdir(pstrPath) if strFile.endswith('.json')
+    ]
+    
+    # parallelize the import process
+    with concurrent.futures.ThreadPoolExecutor() as objExecutor:
+        # process each JSON file concurrently
+        lstDataFrames = list(objExecutor.map(dtfProcessJSON, lstJSONFiles))
+
+    # merge all data frames
+    dtfOut = dtfMergeDataFrames(lstDataFrames)
+
+    return dtfOut
+
+# %% run the process
+if __name__ == '__main__':
+    dtfImport = dtfThreading(strPathJSON)
